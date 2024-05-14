74ccc703fe56ab4a66af6f0dcb1e37d035e97176
EricB <eric.bart65000@gmail.com>
2024-05-14 21:58:38 +0200

    Ajout service récupération produits & Amélioration visibilité site

---
+++ src/main/java/fr/miage/supermarket/dao/ProduitDAO.java
@@ -0,0 +1,64 @@
+package fr.miage.supermarket.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import fr.miage.supermarket.models.Produit;
+import fr.miage.supermarket.utils.HibernateUtil;
+
+/**
+ * Classe de gestion des données pour les produits
+ * @author EricB
+ */
+public class ProduitDAO {
+
+	/**
+	 * Se charge d'enregistrer une liste de produits
+	 * @param produitsToSave les produits à enregistrer
+	 * @author EricB
+	 */
+	public void registerProduits(List<Produit> produitsToSave) {		
+		//Récupération de la session 
+		SessionFactory sessionFactory = HibernateUtil.getSessionAnnotationFactory();
+		Session session = sessionFactory.getCurrentSession();
+		
+		session.beginTransaction();
+		for(Produit prd: produitsToSave) {
+			session.merge(prd);
+		}
+		session.getTransaction().commit();
+		
+		//Ferme la session
+		session.close();
+	}
+	
+	/**
+	 * Se charge de renvoyer l'entièreté des produits présents en base
+	 * @return la liste des produits
+	 */
+	public List<Produit> getAllProduits() {
+		SessionFactory sessionFactory = HibernateUtil.getSessionAnnotationFactory();
+		Session session = sessionFactory.getCurrentSession();
+		
+		session.beginTransaction();
+		
+		try {
+			Query<Produit> query = session.createQuery("FROM Produit", Produit.class);
+			List<Produit> produits = query.getResultList();
+			
+			session.getTransaction().commit();
+			
+			return produits;
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			e.printStackTrace();
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+	
+}
---
+++ src/main/webapp/jsp/index.jsp
@@ -0,0 +1,24 @@
+<%@page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"
+	isELIgnored="false"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<jsp:include page="/jsp/header.jsp">
+	<jsp:param name="title" value="Accueil" />
+</jsp:include>
+<link href="css/accueil.css" rel="stylesheet" type="text/css" />
+<title>Accueil</title>
+</head>
+<body>
+	<%@ include file="navbar.jsp"%>
+	<h1>Accueil</h1>
+
+	<div class="search-bar">
+		<input type="text" placeholder="Rechercher...">
+		<button type="button">Rechercher</button>
+	</div>
+	<div class="article-container" id="article-container"></div>
+	<script src="javascript/produits.js"></script>
+</body>
+</html>
