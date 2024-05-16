291fec5c9abfc8a6045b4c540612b62ad38a5d57
3Ravaka <120034136+3Ravaka@users.noreply.github.com>
2024-05-16 11:48:29 +0200

    conflist

---
+++ src/main/webapp/javascript/produits.js
@@ -0,0 +1,58 @@
+const panier = new Map()
+function chargerProduits() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if(this.readyState == 4 && this.status == 200) {
+			traiterReponse(this);
+		}
+	}
+	xhttp.open("GET", "recupererProduits", true);
+	xhttp.send();
+}
+
+function traiterReponse(xml) {
+	var xmlDoc = xml.responseXML;
+	var produits = xmlDoc.getElementsByTagName("items");
+	var carteContainer = document.getElementById("article-container");
+	
+	for(var i = 0; i < produits.length; i++) {
+		var produit = produits[i];
+		var descriptionCourte = produit.getElementsByTagName("descriptionCourte")[0].childNodes[0].nodeValue;
+		var marque = produit.getElementsByTagName("marque")[0].childNodes[0].nodeValue;
+		var ean = produit.getElementsByTagName("ean")[0].childNodes[0].nodeValue;
+	
+		var carteHTML = '<div class="article-card">';
+		carteHTML += '<h2>' + descriptionCourte + '</h2>';
+		carteHTML += '<p>Marque: ' + marque + '</p>';
+		carteHTML += '<div><input type="number" id="'+ean+'" name="qte" min="0" value="0"><button onclick="ajouterProduit('+ean+')">Ajouter au panier</button></div>'
+		carteHTML += '</div>';
+		
+		// Ajout de la carte au conteneur
+        carteContainer.innerHTML += carteHTML;
+	}
+}
+
+function ajouterProduit(x){
+	var key = x.toString()
+	var valueAdded = parseInt(document.getElementById(x).value)
+	
+	if (panier.get(key) == null){
+		panier.set(key, valueAdded)
+	} else{
+		panier.set(key, panier.get(key)+valueAdded)
+	}
+	
+	console.log(panier)
+	var totalArticles = 0
+	for (let [key, value] of  panier.entries()) {
+    	totalArticles+=value
+	}
+	document.getElementById("panier-compteur").innerHTML = "Nb articles: "+totalArticles
+	
+	
+}
+
+// Appel de la fonction pour charger les produits lors du chargement de la page
+window.onload = function() {
+    chargerProduits();
+};
